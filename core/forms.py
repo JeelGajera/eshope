288c8c7a86c073732c6a7c342f49551f60f6711b
Jeel Gajera <jeelgajera00@gmail.com>
2023-12-12 13:36:15 +0530

    feat: auth, ui

---
+++ core/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'type your username',
+        'is_required': True,
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+    }))
+        
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'name@company.com',
+        'is_required': True,
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "••••••••",
+        'is_required': True,
+        'class': "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "••••••••",
+        'is_required': True,
+        'class': "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    }))
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'type your username',
+        'is_required': True,
+        'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "••••••••",
+        'is_required': True,
+        'class': "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    }))
--- core/views.py
+++ core/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
+
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:5]
     categories = Category.objects.all()
     return render(request, 'core/index.html', {'items': items, 'categories': categories})
 
+
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            password1 = request.POST.get('password1')
+            password2 = request.POST.get('password2')
+            if password1 == password2:
+                form.save()
+
+            return redirect('/login')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {'form': form})
